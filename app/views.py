a9ad8c84caa60700676cbb22d0e6b13b89855711
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-28 16:32:22 +0300

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from app import app
+from .request import get_movies, get_movie
+
+
+#Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+
+    '''
+    popular_movies = get_movies('popular')
+    upcoming_movies = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title, popular = popular_movies, upcoming_movies = upcoming_movies, now_showing = now_showing_movie)
+
+@app.route('/movies/<int:movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(movie_id)
+    title = f'{movie.title}'
+    return render_template('movies.html',movie = movie)
