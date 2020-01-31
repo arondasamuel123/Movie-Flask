11054c58a5d8f09101780698ad4234d602a83757
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-31 08:19:32 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()     
 #Initializing application 
-app = Flask(__name__,instance_relative_config = True)
+
 
 # Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
+    app = Flask(__name__)
 
-bootstrap = Bootstrap(app)
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-from app import views 
-from app import error
+    from .request import configure_request
+    configure_request(app)
+
+    return app
+
+
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='e5f7c5c4815205f214f40873a6b977cb'
+export SECRET_KEY='ba15f7eaa94e46aef7bc71303bd57f58'
+
+python3.6 manage.py serve
